2fede69b85014247ba627f20fb8049874b38250c
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-02-11 22:42:57 -0300

    Project created

---
+++ backend/src/main/java/com/abrantes/pagina_de_vendas/PaginaDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.abrantes.pagina_de_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaDeVendasApplication.class, args);
+	}
+
+}
