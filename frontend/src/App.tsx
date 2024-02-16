9d4d58ec148f245959e7b9bf46123c79c1640c2c
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-02-16 19:32:49 -0300

    creating img folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>  
-    
+    <div>
       <h1 className="text-primary"> Olá mundo </h1>
+      <NavBar />
+      <NavBar />
+      <NavBar />
+      <NavBar />
 
     </div>
   );
