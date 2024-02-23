342d21cb5af34f59fd36c06165f6b8a8977187d1
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-02-23 15:30:19 -0300

    Basic static components

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
                     
-                img src={ImgDsDark} alt="DevSuperior" width="120";
+                         <img src={ImgDsDark} alt="DevSuperior" width="120" />
 
                 </nav>
 
