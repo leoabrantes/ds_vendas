e3f938b9c9c85fb8decb376574dd72df86d13b8d
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-03-08 21:07:59 -0300

    Pagination

---
+++ backend/src/main/java/com/abrantes/pagina_de_vendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.abrantes.pagina_de_vendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.abrantes.pagina_de_vendas.dto.SaleDTO;
+import com.abrantes.pagina_de_vendas.entities.Sale;
+import com.abrantes.pagina_de_vendas.repositories.SaleRepository;
+import com.abrantes.pagina_de_vendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
